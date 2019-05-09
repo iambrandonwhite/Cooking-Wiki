cdd345b36af4c73b680e38113e93cfc40b552a51
1curtislee <1curtislee@gmail.com>
2019-05-08 19:24:25 -0700

    handlebars updates

--- models/index.js
+++ models/index.js
@@ -1,13 +1,26 @@
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
 
+// var path = require("path");
+// var fs = require("fs");
+// var Sequelize = require("sequelize");
+// var basename = path.basename(module.filename);
+var config = require(__dirname + "/../config/config.json")[env];
+// var db = {};
+
+var recipes = 
+  // this info will come from mysql db entries:
+  {
+    title: "Best scrambled eggs",
+    servings: 4,
+    time: "00:30",
+  };
+
+module.exports = recipes;
+
+// from example file:
+/*
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
@@ -40,3 +53,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+*/
