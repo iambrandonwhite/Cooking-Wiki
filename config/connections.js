5512db36843883037ebe57d6857b745785efb31c
coachO88 <rangel52088@gmail.com>
2019-05-08 19:24:48 -0700

    Updated Connection Files

---
+++ config/connections.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+ var connection = mysql.createConnection({
+    host: 'pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    database: 'zbvnutgh6ya4wwus;',
+    user: 'un3w9shcb8vsetqf',
+    password: 'o5pjtqt2o1z34ud0'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
---
+++ models/recipes.js
@@ -0,0 +1,22 @@
+// Global
+const orm = require("../config/orm.js");
+// Burger
+const recipe = {
+    select: function(cb){
+        orm.selectAll("recipes",function(res){
+            cb(res);
+        });
+    },
+    create: function(cols,vals,cb){
+        orm.insertOne("recipes",cols,vals,function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals,condition,cb){
+        orm.updateOne("recipes",objColVals,condition,function(res){
+            cb(res);
+        });
+    }
+};
+// Export Burger
+module.exports = recipes;
