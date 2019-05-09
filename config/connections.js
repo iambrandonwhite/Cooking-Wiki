99672ea04bc5c2e03ca765d4dec19a481040fc05
coachO88 <rangel52088@gmail.com>
2019-05-08 20:08:26 -0700

    Finally able to connect to Database

--- config/connections.js
+++ config/connections.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
  var connection = mysql.createConnection({
     host: 'pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    database: 'zbvnutgh6ya4wwus;',
+    database: 'zbvnutgh6ya4wwus',
     user: 'un3w9shcb8vsetqf',
-    password: 'o5pjtqt2o1z34ud0'
+    password: 'o5pjtqt2o1z34ud0',
+    port: 3306
 });
 
 connection.connect(function(err) {
--- server.js
+++ server.js
@@ -1,47 +1,21 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const methodOverride = require('method-override');
+const routes = require("./controllers/recipes_controller");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-var db = require("./models");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine","handlebars");
+app.use("/", routes);
+app.use(express.static('public'));
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.listen(PORT,function(){
+    console.log("App now listening at localhost:" + PORT);
 });
-
-module.exports = app;
