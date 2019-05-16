89b0438f529e54b1a92740d5118eb03703c90866
1curtislee <1curtislee@gmail.com>
2019-05-15 21:28:47 -0700

    0515 class end

--- public/js/logic.js
+++ public/js/logic.js
@@ -2,10 +2,15 @@
 var searchButton = $("#searchButton");
 var addButton = $("#addButton");
 var newRecipeSubmit = $("#newRecipeSubmit");
+var deleteRecipeButton = $(".deleteRecipe");
 
 searchButton.on("click", function() {
   let query = $("#searchInput").val().trim();
   // search with "query"
+});
+
+deleteRecipeButton.on("click", function(){
+  alert("Are you sure you want to delete this recipe?");
 });
 
 
